2e0c4e31551a10bbf965f845355e66e1d3664bae
Wahid Ali <wahid.imcb@gmail.com>
2026-06-24 20:50:27 +0500

    more explaination about the stings in python

---
+++ more_epl_string.py
@@ -0,0 +1,14 @@
+name = 'Wahid' 
+print(name) #string
+# but if we want to print a message we can use """ code """
+message = """This is a multi-line string.
+It can span multiple lines."""
+print(message)
+# useful function for getting the length of string is len() function
+course = "Python Programming"
+print(len(course)) #length of string
+# also string with index 
+print(course[0]) #first character of string
+print(course[-1]) #last character of string
+print(course[-1]) #last character of string
+print(course[0:6]) #first 6 characters of string
