0b79d85e6953721ff7ba19559a3b4f5748992343
Wahid Ali <wahid.imcb@gmail.com>
2026-06-24 22:18:04 +0500

    import math

--- numbers.py
+++ numbers.py
@@ -1,3 +1,4 @@
+import math
 x = 1 # Integer
 y = 2.5 # Float
 z = 1 + 2.5 # Result will be a Float
@@ -21,3 +22,7 @@ print(10 ** 3) # this will return 10 raised to the power of 3
 x = 10
 x += 3 # this is equivalent to x = x + 3    
 print(x) # this will print 13
+print(round(2.9)) # this will round the number to the nearest integer
+print(abs(-2.9)) # this will return the absolute value of the number
+print(math.ceil(2.9)) # this will return the smallest integer greater than or equal to the number
+print(math.floor(2.9)) # this will return the largest integer less than or equal to the number
