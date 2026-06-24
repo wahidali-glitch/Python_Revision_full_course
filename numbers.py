0674e09aa5ed92b95ec6cc70e75c50e02ee2623c
Wahid Ali <wahid.imcb@gmail.com>
2026-06-24 21:53:10 +0500

    Complex

--- numbers.py
+++ numbers.py
@@ -4,3 +4,10 @@ z = 1 + 2.5 # Result will be a Float
 print(x) # Integer
 print(y) # Float
 print(z) # Float
+
+# x = a + bi where a is the real part and b is the imaginary part pi for I
+# complex numbers are used in advanced mathematics and engineering applications
+a = 2 + 3j
+b = 1 + 2j
+c = a + b
+print(c) # Complex number
