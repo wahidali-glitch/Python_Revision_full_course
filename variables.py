991622f202e4545f836949a13e7c670b2ce78551
Wahid Ali <wahid.imcb@gmail.com>
2026-06-24 20:31:15 +0500

    Variables Examples

---
+++ variables.py
@@ -0,0 +1,15 @@
+student_count = 1000
+print(student_count)
+# bulid-in premitives types
+# numbers, strings, boolean, list, tuple, set, dictionary
+rating = 4.9
+print(rating) #floating
+
+is_published = True
+print(is_published) #boolean
+# python is a case sensitive language lowercase and uppercase letters are different
+# boolean always start with a capital letter True or False
+
+course_name = "Python Programming"
+print(course_name) #string 
+#used to express text data, can be defined using single or double quotes
