ac08692e19adea45dfad73e4a4b5699abe5bf9f0
Wahid Ali <wahid.imcb@gmail.com>
2026-06-25 12:08:48 +0500

    if_else

---
+++ comparison_op.py
@@ -0,0 +1,8 @@
+#coditional statements
+temperature = int(input("Enter the temperature: "))
+if temperature > 30:
+    print("It's a hot day") 
+    print("Drink plenty of water")
+elif temperature > 20:
+    print("It's a nice day")
+print("Enjoy your day")
