ac5ac841202294024f0725225cb17a6c80a6e302
Wahid Ali <wahid.imcb@gmail.com>
2026-06-25 12:31:53 +0500

    chain comparision

--- clean_code.py
+++ clean_code.py
@@ -5,3 +5,10 @@ else:
     print("You are not old enough to vote") 
     print("Please wait until you are 18.")
 
+#chaining comparison operators
+
+age = int(input("Enter your age: "))
+if 18 <= age < 65:
+    print("You are eligible to work")
+else:
+    print("You are not eligible to work")
