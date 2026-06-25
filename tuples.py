9cda434a8c523de9d9d232ca0371745663fa2237
Wahid Ali <wahid.imcb@gmail.com>
2026-06-25 16:07:37 +0500

    understanding of tuple in python

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# tuples are type of data sturucture in python used to store collections of onject.
+# It's like a list but there is one key difference 
+# tuples are immutable it means once you create a tuple you cna't inert and update the tuple
+# when we need to store a data which we doesn't want any modification we can use tuples
+student = ("Ali",123,"A")
+print(student)
+print("Name : ",student[0])
+print('Roll Number: ',student[1])
+print('Grade: ',student[2])
