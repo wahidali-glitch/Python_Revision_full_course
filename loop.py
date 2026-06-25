f2d6b1691d0565ee8b08ddad3b33730a07ce403f
Wahid Ali <wahid.imcb@gmail.com>
2026-06-25 12:41:18 +0500

    loop specially for loop

---
+++ loop.py
@@ -0,0 +1,8 @@
+# in basic we use loops to repeat a block of code multiple times. In python we have two types of loops: for loop and while loop.
+for num in range(5):
+    print(num)
+for message in ["Hello", "World", "Python"]:
+    print(message)
+for i in range(5):
+    for j in range(3):
+        print(f"i: {i}, j: {j}")
