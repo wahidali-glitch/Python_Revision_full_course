7b88ad7fba6e6d7efc6806397160087f332bcc82
Wahid Ali <wahid.imcb@gmail.com>
2026-06-25 13:55:57 +0500

    functions with varibale arguments

--- function.py
+++ function.py
@@ -18,3 +18,12 @@ def multiply(a, b):
     return a * b  # This will return the product of a and b to the caller.
 result = multiply(5, 3)
 print("The product is:", result)  # This will print the result of the multiply function
+
+#function with multiple return values
+def calculate(a, b):
+    sum_result = a + b
+    product_result = a * b
+    return sum_result, product_result  # This will return both the sum and product of a and b to the caller.
+sum, product = calculate(5, 3)
+print("The sum is:", sum)  # This will print the sum result of the calculate function.
+print("The product is:", product)  # This will print the product result of the calculate function.
