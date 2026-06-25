2d45a9c749eec36ff4d813d6713c96f6001c4db0
Wahid Ali <wahid.imcb@gmail.com>
2026-06-25 13:33:22 +0500

    while loop

--- loop.py
+++ loop.py
@@ -23,3 +23,19 @@ for a in range(5):
 # the concept of nested loops is that we have a loop inside another loop. The inner loop will run completely for each iteration of the outer loop. In this example, for each value of a from 0 to 4, the inner loop will run 3 times, printing the current values of a and b.
 print(type(5))
 print(type (range(5)))
+
+number = 100
+while number > 0:
+    print(number)
+    number //= 2  # This is equivalent to number = number // 2, which divides the number by 2 and updates it.
+    command = ""
+    while command != "quit":
+        command = input("Enter a command: ")
+        print("ECHO", command)
+        break  # This break statement will exit the inner while loop after the first iteration, regardless of the input. If you want to allow multiple commands until "quit" is entered, you should remove this break statement.
+    # concept of infinite loop is that the loop will continue to run indefinitely until a certain condition is met. In this case, the inner while loop will keep asking for a command until the user types "quit".
+    while True:
+        command = input("Enter a command: ")
+        if command == "quit":
+            break
+        print("ECHO", command)
