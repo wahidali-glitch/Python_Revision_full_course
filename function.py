e21471f9fb8f672dc8fc0b6f8877a555d52c5d1a
Wahid Ali <wahid.imcb@gmail.com>
2026-06-25 13:52:48 +0500

    return function

--- function.py
+++ function.py
@@ -2,3 +2,19 @@
 def greet():
     print("Hello, welcome to the Python course!")
 greet()  # This will call the greet function and print the welcome message.
+def salam(name):
+    print(f"Hello {name}, welcome to the Python course!")
+print("Enter your name:")
+name = input()  # This will take user input and store it in the variable user  
+salam(name)  # This will call the salam function and print a personalized welcome message.
+# Two types of functions in python: built-in functions and user-defined functions. Built-in functions are those that are already defined in Python, such as print(), len(), and type(). User-defined functions are those that you create yourself, like greet() and salam() in this example.
+# in user defined functions, we can also pass parameters to the function. Parameters are variables that are passed to the function when it is called. In the salam() function, name is a parameter that is passed to the function when it is called.
+# we can get the return value of a function by using the return statement. The return statement is used to exit a function and return a value to the caller. Here's an example:
+def add(a, b):
+    return a + b  # This will return the sum of a and b to the caller.
+result = add(5, 3)
+print("The sum is:", result)  # This will print the result of the add function.
+def multiply(a, b):
+    return a * b  # This will return the product of a and b to the caller.
+result = multiply(5, 3)
+print("The product is:", result)  # This will print the result of the multiply function
