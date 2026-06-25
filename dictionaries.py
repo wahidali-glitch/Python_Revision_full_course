aa0005ed18d40895b3b740549b57059f2ab29434
Wahid Ali <wahid.imcb@gmail.com>
2026-06-25 17:06:36 +0500

    information about dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+# A dictionary in python is like a real-life dictionary 
+# it has keys and values
+# keys are like words and values are like the meaning of the words
+# Why do we use dictionaries
+# They allow us to store and retreive data quickly
+# e.g:
+student = {
+    "name": "Ali",
+    "roll_number": 123,
+    "grade": "A"}
+student["roll_number"]=23
+print(student["name"]) # updating roll number
+print(student["roll_number"])
